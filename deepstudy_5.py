66aade0eff16c64385d117f1502a02939d005111
L-jac <3137501630@qq.com>
2023-03-23 00:09:12 +0800

    学习进度，误差反向传播法5.6.7 Affine/Softmax层的实现

--- deepstudy_5.py
+++ deepstudy_5.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from common.functions import softmax, cross_entropy_error
 
 # TODO 深度学习与python——5 误差反向传播法
 
@@ -85,4 +85,43 @@ class Sigmoid:
 
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+# TODO Affine实现
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        # .T转置
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+
+
+# TODO
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 损失
+        self.y = None # softmax的输出
+        self.t = None # 监督数据（one-hot vector）
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
         return dx

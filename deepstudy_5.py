d989a456781832882b6ad929d652e50ed65a5ba5
L-jac <3137501630@qq.com>
2023-03-23 17:45:30 +0800

    学习进度，误差反向传播法5.7.4

--- deepstudy_5.py
+++ deepstudy_5.py
@@ -1,6 +1,7 @@
 import numpy as np
 from common.functions import softmax, cross_entropy_error
 
+
 # TODO 深度学习与python——5 误差反向传播法
 
 # TODO 乘法层的实现 ch05/layer_naive.py, ch05/buy_apple.py
@@ -110,18 +111,27 @@ class Affine:
         return dx
 
 
-# TODO
+# TODO Softmax-with-Loss层
 class SoftmaxWithLoss:
     def __init__(self):
-        self.loss = None # 损失
-        self.y = None # softmax的输出
-        self.t = None # 监督数据（one-hot vector）
+        self.loss = None  # 损失
+        self.y = None  # softmax的输出
+        self.t = None  # 监督数据（one-hot vector）
+
     def forward(self, x, t):
         self.t = t
         self.y = softmax(x)
         self.loss = cross_entropy_error(self.y, self.t)
         return self.loss
+
     def backward(self, dout=1):
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size
         return dx
+
+# TODO 误差反向传播法的梯度确认ch05/gradient_check.py
+# 数值微分的优点是实现简单，因此，一般情况下不太容易出错。
+# 而误差反向传播法的实现很复杂，容易出错。
+# 所以比较数值微分和误差反向传播法的结果，以确认误差反向传播法的实现是否正确。
+# 确认数值微分求出的梯度结果和误差反向传播法求出的结果是否一致
+# (严格地讲，是非常相近)的操作称为梯度确认(gradient check)。
